e975a937863ea55fcbb89a206fb2c24e6a5fe5d8
ardamoin <ardamoin2@gmail.com>
2023-12-26 21:51:46 -0500

    Add useEffect hook to app component to check whether access token cookie exists and if it does, decode the cookie and update user state in redux using info from the decoded cookie.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,25 @@ import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp";
 import GrantMembership from "./components/GrantMembership";
 import NotFound from "./components/NotFound";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { userActions } from "./store/user-slice";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const accessTokenCookie = document.cookie
+      .split("; ")
+      .find((cookie) => cookie.startsWith("access-token="));
+
+    if (accessTokenCookie) {
+      const accessToken = accessTokenCookie.replace("access-token=", "");
+      const decodedCookie = jwtDecode(accessToken);
+      dispatch(userActions.login(decodedCookie));
+    }
+  }, [dispatch]);
   return (
     <div>
       <Header />
