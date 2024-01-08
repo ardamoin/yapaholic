f951f4111c361d14caf45672ea83291f9c8abe48
ardamoin <ardamoin2@gmail.com>
2024-01-08 16:40:33 -0500

    Connect new message form with the backend

--- client/src/components/NewMessage.jsx
+++ client/src/components/NewMessage.jsx
@@ -1,17 +1,59 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import ErrorPage from "./ErrorPage";
 
 const NewMessage = () => {
   const [charCount, setCharCount] = useState(0);
   const userName = useSelector((state) => state.user.name);
+  const userId = useSelector((state) => state.user.id);
 
   const charCountHandler = (event) => {
     setCharCount(event.target.value.length);
   };
 
-  const formSubmitHandler = (event) => {
+  const formSubmitHandler = async (event) => {
     event.preventDefault();
+
+    const messageFormData = new FormData(event.target);
+    const title = messageFormData.get("title");
+    const text = messageFormData.get("text");
+
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/messages/create`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            user_id: userId,
+            title,
+            text,
+          }),
+          credentials: "include",
+        }
+      );
+
+      const responseData = await response.json();
+      console.log(responseData);
+
+      if (response.ok) {
+        alert(responseData.message);
+        event.target.reset();
+      } else {
+        alert(responseData.error[0].msg);
+      }
+    } catch (error) {
+      console.log(error);
+      alert(error.message);
+    }
   };
+
+  if (typeof userId !== "number") {
+    return (
+      <ErrorPage errorMessage="Please log in before creating a message..." />
+    );
+  }
+
   return (
     <div className="flex flex-col justify-center items-center mt-16">
       <div className="flex flex-col items-start gap-5">
@@ -48,11 +90,12 @@ const NewMessage = () => {
               name="text"
               className="border rounded h-40 px-1 resize-none"
               cols={50}
-              required
               maxLength={255}
               onChange={charCountHandler}
             />
-            <span className="text-xs text-gray-400 self-end">{charCount}/255</span>
+            <span className="text-xs text-gray-400 self-end">
+              {charCount}/255
+            </span>
           </div>
           <button className="flex w-full justify-center items-center h-12 rounded bg-purple-pink text-white mt-4 hover:brightness-75 duration-75 disabled:brightness-50">
             SUBMIT
